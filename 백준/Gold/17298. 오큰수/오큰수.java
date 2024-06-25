7d0cc76d42932668295b493d93cf3a5df69d8815
박예림 <85860891+yerim216@users.noreply.github.com>
2024-06-25 15:25:15 +0900

    [Gold IV] Title: 오큰수, Time: 1032 ms, Memory: 156028 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,47 +1,37 @@
-import java.awt.print.Pageable;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main {
 
-    public static void solution() throws Exception {
+public class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
 
         int n = Integer.parseInt(st.nextToken());
-        int[] nums = new int[n];
-        int[] answer = new int[n];
-        //List<Integer> answer = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
+        int[] arr = new int[n];
+        int[] result = new int[n];
+
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
-            int num = Integer.parseInt(st.nextToken());
-            nums[i] = num;
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            result[i] = -1;  
         }
-        for(int i=n-1; i>=0; i--){
-            while(!stack.isEmpty() && stack.peek()<=nums[i]){
-                stack.pop();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                result[stack.pop()] = arr[i];
             }
-            answer[i] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(nums[i]);
-        }
-        for(int i=0; i<n; i++){
-            sb.append(answer[i]).append(" ");
+            stack.push(i);
         }
 
+        for (int i = 0; i < n; i++) {
+            sb.append(result[i]).append(" ");
+        }
 
-        System.out.println(sb);
+        System.out.println(sb.toString().trim());
     }
-
-
-    public static void main(String[] args) throws Exception {
-        solution();
-    }
-
 }
