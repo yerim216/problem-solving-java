4cb43d0d3356c77cc5621c51ca65def7ca2c1713
박예림 <85860891+yerim216@users.noreply.github.com>
2024-08-04 23:31:39 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,95 +1,61 @@
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-
 public class Main {
 
-    public static class Pair {
-        int x, y;
-
-        public Pair(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-        public int getX(){
-            return x;
-        }
-
-        public int getY(){
-            return y;
-        }
-
-        public void setX(int x){
-            this.x = x;
-        }
-        public void setY(int y){
-            this.y = y;
-        }
-    }
-    public static void solution() throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Queue<Pair> queue = new LinkedList<>();
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, -1, 0, 1};
+
+        int[][] board = new int[n][m];
         int[][] dist = new int[n][m];
-        int[][] arr = new int[n][m];
+        Queue<int[]> queue = new LinkedList<>();
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
 
-
-        for(int i=0; i<n; i++){
-            String s = br.readLine();
-            for(int j=0; j<m; j++){
-                if(s.charAt(j)=='1'){
-                    arr[i][j]=1;
-                } else {
-                    arr[i][j]=0;
-                }
-                dist[i][j]=-1;
-            }
-        }
 
         for(int i=0; i<n; i++) {
+            String s = br.readLine();
             for (int j = 0; j < m; j++) {
-                if(arr[i][j]==0 || dist[i][j]!=-1) continue;
-                queue.add(new Pair(i, j));
-                dist[i][j]=1;
-                while (!queue.isEmpty()) {
-                    int x = queue.peek().getX();
-                    int y = queue.peek().getY();
-                    queue.poll();
-                    for (int dir = 0; dir < 4; dir++) {
-                        int nx = x + dx[dir];
-                        int ny = y + dy[dir];
-                        if (nx < 0 || nx >= n || ny < 0 || ny >= m)
-                            continue;
-                        if (dist[nx][ny]==-1 && arr[nx][ny] == 1) {
-                            queue.add(new Pair(nx, ny));
-                            dist[nx][ny] = dist[x][y]+1;
-                        }
-
-                    }
-
-
-                }
-
-
-
+                board[i][j] = s.charAt(j)-'0';
+                dist[i][j] = -1;
             }
         }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j]==0 || dist[i][j]!=-1) continue;
+                queue.add(new int[]{i, j});
+                dist[i][j] = 1;
+                while(!queue.isEmpty()){
+                    int[] temp = queue.poll();
+                    int x = temp[0];
+                    int y = temp[1];
+                    for(int dir=0; dir<4; dir++){
+                        int nx = x + dx[dir];
+                        int ny = y + dy[dir];
+                        if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
+                        if(dist[nx][ny]==-1 && board[nx][ny]==1){
+                            queue.add(new int[]{nx, ny});
+                            dist[nx][ny] = dist[x][y]+1;
+                        }
+                    }
+                }
+            }
+        }
+
         System.out.println(dist[n-1][m-1]);
+
+
+
+
     }
-
-
-    public static void main(String[] args) throws Exception {
-        solution();
-    }
-
 }
