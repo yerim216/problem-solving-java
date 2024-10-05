a751ebae3cc39ac47581be79c34ab6cfff696f1b
박예림 <85860891+yerim216@users.noreply.github.com>
2024-10-06 08:44:11 +0900

    [난이도 1] Title: 정수의 길이, Time: 0.06 ms, Memory: 31.18 MB -BaekjoonHub

---
+++ goormlevel/173089/1. 정수의 길이/정수의 길이.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String s = st.nextToken();
+		System.out.println(s.length());
+		//String input = br.readLine();
+		//System.out.println("Hello Goorm! Your input is " + input);
+	}
+}
