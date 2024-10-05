4f2d64acda0ee7d3ad7f23c1a249c39d51b8b4f2
박예림 <85860891+yerim216@users.noreply.github.com>
2024-10-05 19:16:02 +0900

    [level 2] Title: 프로세스, Time: 1.24 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,28 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] priorities, int location) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=0; i<priorities.length; i++){
-            pq.add(priorities[i]);
-        }
         int answer = 0;
-        
-        while(!pq.isEmpty()){
-            for(int i=0; i<priorities.length; i++){
-                if(priorities[i]==pq.peek()){
-                    answer++;
-                    if(i==location){
-                        return answer;
-                    }
-                    pq.poll();
-                }
-            }
-        }
-        
-        
-        
-        //int answer = 0;
         return answer;
     }
 }
