e259e74fd0d06f0a973d9d6472e9016a55ce6a14
YERIM PARK <85860891+yerim216@users.noreply.github.com>
2025-04-19 13:53:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 106.82 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,32 +1,42 @@
 import java.util.*;
 class Solution {
-    //100,000,000,00
-    //n^2 절대 X. O(n)에 끝내야 함. 
+
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
         for(String s : participant){
-            if(map.containsKey(s)){
-                int curValue = map.get(s);
-                map.put(s, ++curValue);
-            } else{
-                map.put(s, 1);
-            }
+            map.put(s, map.getOrDefault(s, 0)+1);
         }
-        String answer = "";
-        
         for(String s : completion){
-            if(map.containsKey(s)){
-                int curValue = map.get(s);
-                if(curValue==1){
-                    map.remove(s);
-                } else{
-                    map.put(s, --curValue);
-                }
+            map.put(s, map.get(s)-1);
+        }
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            if(entry.getValue()>0){
+                return entry.getKey();
             }
         }
-        for(String s : map.keySet()){
-            answer = s;
-        }
+//         for(String s : participant){
+//             if(map.containsKey(s)){
+//                 int curValue = map.get(s);
+//                 map.put(s, ++curValue);
+//             } else{
+//                 map.put(s, 1);
+//             }
+//         }
+         String answer = "";
+        
+//         for(String s : completion){
+//             if(map.containsKey(s)){
+//                 int curValue = map.get(s);
+//                 if(curValue==1){
+//                     map.remove(s);
+//                 } else{
+//                     map.put(s, --curValue);
+//                 }
+//             }
+//         }
+//         for(String s : map.keySet()){
+//             answer = s;
+//         }
         return answer;
         
     }
