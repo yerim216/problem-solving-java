0f4f11585daa1055d95487189d7b096ae1b65441
YERIM PARK <85860891+yerim216@users.noreply.github.com>
2025-04-19 14:26:59 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 56.25 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
 
 ### 성능 요약
 
-메모리: 117 MB, 시간: 56.88 ms
+메모리: 112 MB, 시간: 56.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 05일 15:06:15
+2025년 04월 19일 14:26:58
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,22 +1,41 @@
 import java.util.*;
-import java.io.*;
 
 public class Solution {
-    public int[] solution(int[] arr) {
-        List<Integer> list = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
-        //boolean[] status = new boolean[10];
-        for(int i=0; i<arr.length; i++){
-            if(stack.isEmpty() || stack.peek()!=arr[i]){
-                list.add(arr[i]);
-                stack.add(arr[i]);
+    public int[] solution(int []arr) {
+        //int[] answer = {};
+        //Queue<Integer> q = new LinkedList<>();
+        //Deque<Integer> dq = new LinkedList<>();
+        Stack<Integer> st = new Stack<>();
+        for(int num : arr){
+            if(st.isEmpty()){
+                st.add(num);
+            } else if(st.peek()!=num){
+                st.add(num);
             }
         }
-        int[] answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++){
-            answer[i] = list.get(i);
+        int[] ans = new int[st.size()];
+        for(int i=st.size()-1; i>=0; i--){
+            ans[i] = st.pop();
         }
-        return answer;
-     
+        return ans;
+        //return dq.toString();
+        // for(int num : arr){
+        //     if(q.isEmpty()){
+        //         q.add(num);
+        //     } else if(q.peek()!=num){
+        //         q.add(num);
+        //     }
+        // }
+        // System.out.println(q.toString());
+//         int[] answer = new int[q.size()];
+//         int idx = 0;
+//         while(q.isEmpty()){
+//             answer[idx++] = q.poll();
+//         }
+        
+
+        //System.out.println("Hello Java");
+
+       // return answer;
     }
 }
